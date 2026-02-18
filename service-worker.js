6b2c39779f91e4284c3e4bc268805bb7fedfc6e4
muiparsal <muiparakansalak@gmail.com>
2026-02-18 13:25:12 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    SERVICE WORKER – MUSHAF ONLINE (OPTIMIZED)
    =============================== */
 
-const CACHE_VERSION = 'v2.1.0';
+const CACHE_VERSION = 'v2.1.1';
 
 const STATIC_CACHE = `muiquran-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `muiquran-runtime-${CACHE_VERSION}`;
@@ -278,6 +278,7 @@ self.addEventListener('fetch', event => {
   );
 
 });
+
 
 
 
